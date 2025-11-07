50634cf6cc44de8fbd4ec748ad84b0265433b589
milinD537 <milindpithadia22@gmail.com>
2025-11-07 13:14:41 +0530

    cleaned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,35 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerDescription,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-	DrawerTrigger,
-} from "@/components/ui/drawer"
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
 import { cn } from "@/lib/utils"
 import { useEffect, useMemo, useState } from "react"
 import useIsLoaded from "@/hooks/use-is-loaded"
 import { dialogData } from "@/lib/mock"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-
+import dynamic from "next/dynamic"
+const AtProperties = dynamic(
+	() => import("../components/custom/at-properties"),
+	{ ssr: false }
+)
 export default function Home() {
 	const [activeIndex, setActiveIndex] = useState<number[]>([])
-	// const prevActiveIndex = useRef(activeIndex)
 	const [prevActiveIndex, setPrevActiveIndex] = useState<number[]>([])
 	const [dims, setDims] = useState<{ height?: number; width?: number }>({})
 	const [drawerOpen, setDrawerOpen] = useState(false)
 	const [activeNode, setActiveNode] = useState<HTMLDivElement | null>(null)
 	const loaded = useIsLoaded()
-	// const activeRef = useRef<HTMLDivElement>(null)
 	useEffect(() => {
 		console.log(activeNode, activeIndex, loaded)
-
-		if (activeNode) {
-			setDims({
-				height: activeNode.offsetHeight,
-				width: activeNode.offsetWidth,
-			})
-		}
-		// prevActiveIndex.current = activeIndex
-	}, [activeNode])
+		setTimeout(() => {
+			const element = document.getElementById("0-node")
+			if (element) {
+				setDims({
+					height: element.offsetHeight,
+					width: element.offsetWidth,
+				})
+			}
+		}, 100)
+	}, [activeIndex, drawerOpen, loaded])
 	console.log({ dims })
 	useEffect(() => {
 		if (!drawerOpen) {
@@ -50,7 +44,7 @@ export default function Home() {
 
 	function updateActiveIndex(newIndex: number[]) {
 		setPrevActiveIndex(activeIndex)
-		setTimeout(() => setActiveIndex(newIndex), 500)
+		setTimeout(() => setActiveIndex(newIndex), 100)
 	}
 
 	const currentList = useMemo(() => {
@@ -68,8 +62,6 @@ export default function Home() {
 
 		return [currentData || {}, prevData || {}]
 	}, [activeIndex, prevActiveIndex])
-	// index === 0 -> dikhna chaiye + current
-	// index === 1 -> chhupna chaiye + past
 	function isCurrent(index: number) {
 		return index === 0
 	}
@@ -91,58 +83,40 @@ export default function Home() {
 
 	return (
 		<Drawer open={drawerOpen} onOpenChange={setDrawerOpen}>
+			<AtProperties />
 			<DrawerTrigger>Open</DrawerTrigger>
 			<DrawerContent
 				className="transition-[height]! duration-1000 mx-auto w-[min(512px,100%-2rem)] ~mb-12! bottom-12! ~after:bg-transparent! rounded-2xl! items-center overflow-hidden ~after:hidden!"
 				style={{
 					height: dims.height ? dims.height : "auto",
-					// transition: "height 1s ease",
 				}}
 			>
 				{currentList.map((item, index) => (
 					<div
 						key={index}
 						className={cn(
-							"absolute inset-x-0 content-center outline ~transition-[display,visibility,opacity,translate] ~duration-1000 transition-discrete |",
+							"absolute inset-x-0 content-center transition-discrete py-4 |",
 							{
 								"z-10 ~opacity-100 ~block ~visible ~translate-x-0 ~starting-hidden":
-									// index ===
-									// 	activeIndex[activeIndex.length - 1] ||
-									// activeIndex.length === 0,
 									isCurrent(index),
 								"starting-x-full animate-[nextToCurrent_1s_forwards]":
-									isCurrent(index) &&
-									// activeIndex[activeIndex.length - 1]
-									isNext(index),
+									isCurrent(index) && isNext(index),
 								"-starting-x-full animate-[previousToCurrent_1s_forwards]":
-									isCurrent(index) &&
-									// activeIndex[activeIndex.length - 1]
-									isPrevious(index),
+									isCurrent(index) && isPrevious(index),
 								"~opacity-0 ~hidden ~invisible": isPast(index),
-								// activeIndex[activeIndex.length - 1]
-								// activeIndex.length > 0
 								"translate-x-full animate-[currentToNext_1s_forwards]":
-									isPast(index) &&
-									// prevActiveIndex[
-									// 	prevActiveIndex.length - 1
-									// ]
-									isNext(index),
+									isPast(index) && isNext(index),
 								"-translate-x-full animate-[currentToPrevious_1s_forwards]":
-									isPast(index) &&
-									// prevActiveIndex[
-									// 	prevActiveIndex.length - 1
-									// ]
-									isPrevious(index),
+									isPast(index) && isPrevious(index),
 							},
 							prevActiveIndex
 						)}
 						ref={
-							isCurrent(index) &&
-							// activeIndex[activeIndex.length - 1]
-							drawerOpen
+							isCurrent(index) && drawerOpen
 								? setActiveNode
 								: null
 						}
+						id={index + "-node"}
 					>
 						{activeIndex.length > 0 && (
 							<Button
@@ -150,22 +124,27 @@ export default function Home() {
 								onClick={() =>
 									updateActiveIndex(activeIndex.slice(0, -1))
 								}
+								className="px-4 hover:bg-transparent"
 							>
 								<ChevronLeft /> Back
 							</Button>
 						)}
-						<div className="flex flex-col gap-4 w-full">
+						<div className="flex flex-col w-full">
 							{item.children?.map((child, childIndex) => (
 								<Button
 									variant="ghost"
 									key={childIndex}
 									onClick={() => {
+										if (!child.children) {
+											setDrawerOpen(false)
+											return
+										}
 										updateActiveIndex([
 											...activeIndex,
 											childIndex,
 										])
 									}}
-									className="text-xs block text-start w-full h-auto p-0"
+									className="text-xs block text-start w-full h-auto p-0 px-4 py-2 hover:bg-transparent"
 								>
 									<span className="grid gap-2 grid-cols-[auto_1fr_auto] items-start font-medium">
 										{child.icon}
